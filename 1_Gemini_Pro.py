f6ac19b6b792bf54b7892ef42727f9383d85dc7c
SimhaUnleashed <68226056+SimhaUnleashed@users.noreply.github.com>
2024-07-31 01:14:05 +0530

    Update 1_Gemini_Pro.py

--- 1_Gemini_Pro.py
+++ 1_Gemini_Pro.py
@@ -25,6 +25,7 @@ hide_streamlit_style = """
             <style>
             [data-testid="stToolbar"] {visibility: hidden !important;}
             footer {visibility: hidden !important;}
+            .embeddedAppMetaInfoBar_container__DxxL1 {visibility: hidden !important;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
